42ba02629e4c1e293451a5068cd23ace5bfafc4d
wangdejun <1074201799@qq.com>
2016-08-16 20:05:57 +0800

    circle things

--- array_operation_in_js.js
+++ array_operation_in_js.js
@@ -1,17 +1,28 @@
 let myarr = new Array();
 console.log(myarr);
-myarr[0]={name1:"wangdejun"};
+myarr[0]={name1:[1,2,3,4,5,6,7]};
 console.log(myarr);
-myarr[1]={name2:"wangdejunjun"};
+myarr[1]={name2:[8,9,10,11,12,13,14]};
 console.log(myarr);
+myarr[1]={name2:[8,9,10,11,12,13,14]};
+console.log(myarr);
+
+
+myarr={
+	name1:[1,2,3,4,5,6,7],
+	name2:[8,9,10,11,12,13,14],
+	name2:[8,9,10,11,12,13,14],
+}
 /**
  * map() -> i -> item
  */
-console.log(myarr.map((item,i)=>{
+myarr.map((item,i)=>{
 	for(prop in item){
-		//通过数组中对象的key取出来value;
-		return "nihao " + item[prop] + "  " +"index:"+i;
+		//通过数组中对象的key取出来value,value值是一个数组;
+		console.log(prop);
+		item[prop].map((item1,j)=>{
+			//对小数组进行map运算，返回数组中的每一个值
+			console.log(item1);
+		});
 	}
-}));
-
-console.log(myarr);
+});
