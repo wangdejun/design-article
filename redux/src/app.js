5d03293b9a20098f10b10923bae58e66523e91b8
wangdejun <1074201799@qq.com>
2016-08-28 17:05:57 +0800

    change

--- redux/src/app.js
+++ redux/src/app.js
@@ -1,15 +1,17 @@
+// src/app.js
+
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducer';
-import { TodoList } from './containers';
+import { TodoList } from './components';
 
-const store = createStore(reducer);
+const dummyTodos = [
+  { id: 0, isDone: true,  text: 'make components' },
+  { id: 1, isDone: false, text: 'design actions' },
+  { id: 2, isDone: false, text: 'implement reducer' },
+  { id: 3, isDone: false, text: 'connect components' }
+];
 
 render(
-  <Provider store={store}>
-    <TodoList />
-  </Provider>,
+  <TodoList todos={dummyTodos} />,
   document.getElementById('app')
 );
